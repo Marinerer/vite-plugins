50991b29ab6003fb73837a241cb31abf8c013aa9
Meqn <mengqing723@gmail.com>
2024-11-05 08:37:10 +0000

    feat(vanilla): Initialize vanilla plugin

---
+++ packages/vanilla/README.md
@@ -0,0 +1,6 @@
+# vite-plugin-vanilla
+
+Vanilla multi-page web development model based on `Vite.js`.
+
+基于 `Vite.js` 的传统的多页面web开发模式。
+
---
+++ packages/vanilla/index.js
@@ -0,0 +1,13 @@
+const PLUGIN_NAME = "vite-plugin-vanilla"
+
+/**
+ * vanilla plugin
+ * @param {string[]} pagesPatterns pages patterns
+ * @param {object} options options
+ * @returns 
+ */
+export default function createPlugin(pagesPatterns, options) {
+  return {
+    name: "vite-plugin-vanilla",
+  }
+}
